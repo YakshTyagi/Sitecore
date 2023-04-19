9749b6e8f257b9a4f94abe66b491bc01df618248
Yaksh Tyagi <yakshtyagiyt@gmail.com>
2023-04-19 19:15:48 +0530

    First Sitecore commit

---
+++ MSIL_API/Controllers/DealerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSIL_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MSIL_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DealerController : ControllerBase
+    {
+        // GET: api/<DealerController>
+        [HttpGet]
+        public IEnumerable<Dealers> Get()
+        {
+            return new Dealers[] {
+
+                new Dealers { Contacts= "+91-9996642825466", Id = Guid.NewGuid(), Name="abc", Address="Ghaziabad" },
+                new Dealers { Contacts= "+91-9996642825465", Id = Guid.NewGuid(), Name="mon", Address="Noida" },
+                new Dealers { Contacts= "+91-9996642825436", Id = Guid.NewGuid(), Name="xyz", Address="Greatere Noida" }
+            };
+        }
+
+
+    }
+}
---
+++ MSIL_API/Models/CarsVariant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSIL_API.Models
+{
+    public class CarsVariant
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public CarSpecs Specs { get; set; }
+    }
+}
